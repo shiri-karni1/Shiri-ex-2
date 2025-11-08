6daf1c8b49cfa3d80664c68aa6c55375cd8ca973
Shiri Karni <shiri@innovate.huji.ac.il>
2025-11-08 20:15:59 +0200

    changes to css

--- app/demos/tic-tac-toe/page.tsx
+++ app/demos/tic-tac-toe/page.tsx
@@ -38,18 +38,18 @@ export default function Board() {
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
+      <div className={styles.status}>{status}</div>
+      <div className={styles.boardRow}>
         <Square  squareClass={styles.square} value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square squareClass={styles.square} value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square squareClass={styles.square} value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="board-row">
+      <div className={styles.boardRow}>
         <Square squareClass={styles.square} value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square squareClass={styles.square} value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square squareClass={styles.square} value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="board-row">
+      <div className={styles.boardRow}>
         <Square squareClass={styles.square} value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square squareClass={styles.square} value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square squareClass={styles.square} value={squares[8]} onSquareClick={() => handleClick(8)} />
